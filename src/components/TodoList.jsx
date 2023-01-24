3debe1ed85e0179fb68acbbf65bed14f705576fa
james-fleming394 <james.fleming394@gmail.com>
2023-01-24 15:25:18 -0500

    Input and button working for edit and add

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,28 +1,7 @@
 import React from "react";
-import { useState, useEffect, useRef } from "react";
 
-const TodoList = (props) => {
-    const [input, setInput] = useState(props.edit ? props.edit.value : '');
-
-    const inputRef = useRef(null);
-
-    useEffect(() => {
-        inputRef.current.focus();
-    });
-
-    const handleChange = (e) => {
-        setInput(e.target.value);
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        props.onSubmit({
-            id: Math.floor(Math.random() * 10000),
-            text: input
-        });
-        setInput('');
-    };
+const TodoList = () => {
+    
 }
 
 export default TodoList;
